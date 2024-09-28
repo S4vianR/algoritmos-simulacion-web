d2968755e3b16c1e8213fd5d1612c39120fba366
S4vianR <reuben4.com@gmai.com>
2024-09-27 22:59:54 -0500

    Se removió la pagina de equipo

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
     <Header class="w-full h-[7svh]">
-      <nav class="p-5  flex items-center justify-center gap-12">
+      <nav class="flex items-center justify-center gap-12 p-5">
         <h1 class="text-lg font-semibold">
           <nuxt-link to="/">DataDynamos</nuxt-link>
         </h1>
         <ol class="flex items-center justify-center gap-4">
           <li class="transition-transform hover:-translate-y-1">
             <nuxt-link to="/">Inicio</nuxt-link>
-          </li>
-          <li class="transition-transform hover:-translate-y-1">
-            <nuxt-link to="/equipo">Equipo</nuxt-link>
           </li>
         </ol>
       </nav>
